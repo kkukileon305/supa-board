718c85455e164b2bc6414de6a1e9bb3917c89c8e
kkukileon305 <kkukileon305@gmail.com>
2022-12-03 02:13:45 +0900

    title 수정, meta desc 추가

--- app/head.tsx
+++ app/head.tsx
@@ -2,6 +2,7 @@ export default function Head() {
   return (
     <>
       <title>Supa Board</title>
+      <meta name='description' content='연습용 게시판입니다.' />
       <meta content='width=device-width, initial-scale=1' name='viewport' />
       <link rel='icon' href='/favicon.ico' />
     </>
