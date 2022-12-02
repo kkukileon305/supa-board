12a188e086c6f6fb79b7147ceba5cb710b810cdb
kkukileon305 <kkukileon305@gmail.com>
2022-12-03 02:10:16 +0900

    title 수정

--- app/head.tsx
+++ app/head.tsx
@@ -1,7 +1,7 @@
 export default function Head() {
   return (
     <>
-      <title>Plzang</title>
+      <title>Supa Board</title>
       <meta content='width=device-width, initial-scale=1' name='viewport' />
       <link rel='icon' href='/favicon.ico' />
     </>
